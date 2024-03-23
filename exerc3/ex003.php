b29d19ec7e04ef3c984901fc0ba2c9d4b02ca3d2
dansilx <danilosilveira@msn.com>
2024-03-23 11:10:32 -0300

    salvar

---
+++ exerc3/ex003.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex003</title>
+</head>
+<body>
+    <form action="ex003resposta.php" method="post">
+        <label>Calcule dois números:</label><br>
+        <input type="number" name="valor1"><br>
+        <input type="number" name="valor2"><br>
+        <button type="submit">Enviar</button>
+
+    </form>
+</body>
+</html>
