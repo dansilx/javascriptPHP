1f0d034bf452c3d3a81a3d3307dac17d68718c40
dansilx <danilosilveira@msn.com>
2024-03-27 02:35:49 -0300

    salvar

---
+++ exerc4/ex004.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHPex004</title>
+</head>
+<body>
+    <form action="ex004resposta.php" method="post">
+        <label>Calcule a tabuada</label><br>
+        <input type="number" name="valor"><br>
+        <button type="submit">Enviar</button><br>
+        
+    </form>
+</body>
+</html>
