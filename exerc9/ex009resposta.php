9679f0d4121d88bd495cbb39a98f107b5d82c356
dansilx <danilosilveira@msn.com>
2024-03-29 12:26:20 -0300

    salvar

--- exerc9/ex009resposta.php
+++ exerc9/ex009resposta.php
@@ -1,6 +1,26 @@
 <?php   
 
-    $idade = $_POST['idade'];
-
-    $dataAtual = date("d/m/Y");
+    function calcIdade($anoNasc, $anoAtual) {
+        return $idade = $anoAtual - $anoNasc;
+    }
     
+    function diasVividos ($anoNasc, $anoAtual) {
+        return $diasVividos = ($anoAtual -$anoNasc) * 365;
+    }
+
+    function idade25 ($anoNasc, $anoAtual) {
+        return $idade25 = 2025 - $anoNasc;
+    }
+
+    $anoNasc = $_POST['anoNasc'];
+    define("ANO_ATUAL", date("Y"));
+    
+    $idade = calcIdade($anoNasc, ANO_ATUAL);
+
+    $diasVividos = diasVividos($anoNasc, ANO_ATUAL);
+
+    $idade25 = idade25($anoNasc, ANO_ATUAL);
+
+    echo "<p>-- Sua idade é $idade anos --</p>";
+    echo "<p>-- Você já viveu $diasVividos dias --</p>";
+    echo "<p>-- Você terá $idade25 anos em 2025 --</p>";
