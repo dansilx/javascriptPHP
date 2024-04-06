6f63dbd4f89c4449562ef989f1c879cd75661228
dansilx <danilosilveira@msn.com>
2024-04-05 22:09:56 -0300

    salvar

--- mapasOrdenados/processar_media.php
+++ mapasOrdenados/processar_media.php
@@ -4,12 +4,26 @@
     $n2 = $_POST['n2'];
     $n3 = $_POST['n3'];
     $n4 = $_POST['n4'];
+    $faltas = $_POST['faltas'];
+    $media = ($n1 + $n2 + $n3 + $n4) / 4;
 
-    $media = $n1 + $n2 + $n3 + $n4/ 4;
-    // se a nota for abaixo de 6 reprovado
+    // se a nota for abaixo de 6 e qtd de faltas > 10 = reprovado 
     //senão aprovado
-    if ($media >= 6) {
+    if ($media >= 6 && $faltas < 10) {
         echo "Aprovado";
     } else {
         echo "Reprovado";
     }
+
+    $vetor = array(10, 20, 30);
+    echo $vetor[1];
+    $vetor[3] = 40;
+    echo "<br>".$vetor[3];
+    $vetor["tio"] = "tio";
+    echo "Valores do vetor: <br>";
+    foreach($vetor as $v) {
+        echo "$v <br>";
+    }
+
+    print_r($vetor);
+    var_dump($vetor);
