6a6a03fc4af9eaae05a5c9a8eb90a0d285fd8f66
dansilx <danilosilveira@msn.com>
2024-05-18 10:47:06 -0300

    salvar

--- projetodb/funcao.php
+++ projetodb/funcao.php
@@ -14,3 +14,19 @@
             return 0;
         } 
     }
+
+    function inserirProduto($nome, $descricao, $valor, $categoria) {
+        try {
+
+            $sql = "INSERT INTO produto (nome, descricao, valor, categoria_id) VALUES (:nome, :descricao, :valor, :categoria)";
+            $conexao = conectarBanco();
+            $stmt = $conexao->prepare($sql);
+            $stmt->bindValue(":nome", $nome);
+            $stmt->bindValue(":descricao", $descricao);
+            $stmt->bindValue(":valor", $valor);
+            $stmt->bindValue(":categoria", $categoria);
+            return $stmt->execute();
+        } catch (Exception $e) {
+            return 0;
+        }
+    }
--- projetodb/produtos/inserir_produto.php
+++ projetodb/produtos/inserir_produto.php
@@ -3,29 +3,29 @@
 ?>
 
     <h3 class="text-white">Inserir Produto</h3>
-    <form class="bg-dark text-white">
+    <form class="bg-dark text-white" action="" method="POST">
         <div class="row">
             <div class="col">
                 <label for="nome" class="form-label m-2">Informe o nome: </label>
-                <input type="text" class="form-control m-2 bg-secondary text-white" name="nome">
+                <input type="text" class="form-control m-2 bg-secondary text-white" name="nome" required>
             </div>
         </div>
         <div class="row">
             <div class="col">
                 <label for="nome" class="form-label m-2">Informe a descrição: </label>
-                <input type="text" class="form-control m-2 bg-secondary text-white" name="descricao">
+                <input type="text" class="form-control m-2 bg-secondary text-white" name="descricao" required>
             </div>
         </div>
         <div class="row">
             <div class="col">
                 <label for="nome" class="form-label m-2">Informe o valor: </label>
-                <input type="text" class="form-control m-2 bg-secondary text-white" name="valor">
+                <input type="text" class="form-control m-2 bg-secondary text-white" name="valor" required>
             </div>
         </div>
         <div class="rol">
             <div class="col">
                 <label for="categoria" class="form-label m-2 ">Selecione a categoria: </label>
-                <select name="categoria" id="" class="form-select m-2 bg-secondary text-white">
+                <select name="categoria" id="" class="form-select m-2 bg-secondary text-white" required>
                     <?php 
                         $linhas = retornarCategorias();
                         while($l = $linhas->fetch(PDO::FETCH_ASSOC)){
@@ -43,5 +43,19 @@
     </form>
 
 <?php
-
+    if ($_POST) {
+        $nome = $_POST['nome'];
+        $descricao = $_POST['descricao'];
+        $valor = $_POST['valor'];
+        $categoria = $_POST['categoria'];
+        if ($nome != "" && $descricao != "" && $valor != "" && $categoria != "") {
+            if(inserirProduto($nome, $descricao, $valor, $categoria)){
+                echo '<p class="text-white">Registro inserido com sucesso!</p>';
+            } else {
+                echo '<p class="text-white">Erro ao inserir o registro!</p>';
+            }
+        } else {
+            echo '<p class="text-white">Preencha todos os campos!</p>';
+        }
+    }
     require_once("../footer.php");
